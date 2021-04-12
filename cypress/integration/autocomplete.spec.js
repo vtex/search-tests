402bdfe7b6caf82a527eaeab46fb2f07eb317f7e
Thalyta Fabrine <thalyta.trindade@vtex.com.br>
2021-04-12 12:20:30 -0300

    Test filter by brand

--- cypress/integration/autocomplete.spec.js
+++ cypress/integration/autocomplete.spec.js
@@ -8,9 +8,14 @@ context('Autocomplete', () => ***REMOVED***
     cy.visit('/')
 ***REMOVED***)
 
-  it('should open the autocomplete', () => ***REMOVED***
+  it('should render the search bar and the autocomplete should be hidden', () => ***REMOVED***
     cy.get(CONSTANTS.hiddenAutocomplete).should('exist')
     cy.get(CONSTANTS.autocomplete).should('exist')
+    cy.get(CONSTANTS.searchBarContainer).should('exist')
+    cy.get(CONSTANTS.searchBarContainer).should('be.visible')
+***REMOVED***)
+
+  it('should open the autocomplete', () => ***REMOVED***
     cy.get(CONSTANTS.searchBarContainer).click()
     cy.get(CONSTANTS.hiddenAutocomplete).should('not.exist')
     cy.get(CONSTANTS.autocomplete).should('exist')
--- cypress/integration/department-page.spec.js
+++ cypress/integration/department-page.spec.js
@@ -38,5 +38,21 @@ context('Department page', () => ***REMOVED***
     cy.get(CONSTANTS.searchResultItem).should('have.length', 3)
     cy.get(CONSTANTS.breadcrumbLink).should('have.length', 3)
     cy.get(CONSTANTS.breadcrumbLink).eq(2).should('have.text', 'Roupa')
+    cy.get(CONSTANTS.searchTitle).should('have.text', 'Roupa')
+***REMOVED***)
+
+  it('should have three visible brand filters', () => ***REMOVED***
+    cy.get(CONSTANTS.brandFilter).should('exist')
+    cy.get(CONSTANTS.brandFilterItems).should('have.length', 3)
+    cy.get(CONSTANTS.brandFilterItems).should('be.visible')
+***REMOVED***)
+
+  it('should filter by brand', () => ***REMOVED***
+    cy.get(CONSTANTS.brandFilterItems).contains('Mizuno').click()
+    cy.url().should('include', 'map=category-1,category-2,brand')
+    cy.get(CONSTANTS.searchResultItem).should('have.length', 1)
+    cy.get(CONSTANTS.breadcrumbLink).should('have.length', 4)
+    cy.get(CONSTANTS.breadcrumbLink).eq(3).should('have.text', 'Mizuno')
+    cy.get(CONSTANTS.searchTitle).should('have.text', 'Mizuno')
 ***REMOVED***)
 ***REMOVED***)
